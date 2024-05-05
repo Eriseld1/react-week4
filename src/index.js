ec6d03565ca4e91bd00e160e4a685bec7553320d
Eriseld1 <159956782+Eriseld1@users.noreply.github.com>
2024-05-05 11:28:29 -0400

    imported weather app to react

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import WeatherSearch from "./WeatherSearch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<div className="App">
+			<h1>Weather App</h1>
+			<WeatherSearch />
+		</div>
 	</React.StrictMode>
 );
 
